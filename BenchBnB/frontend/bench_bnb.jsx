8ef8cdc7e014cf36f8e2aed32ae65ff6f06a76e5
Omayemi Agbeyegbe <grend500@gmail.com>
2017-08-18 15:02:12 -0400

    Add ApiUtil functions

--- BenchBnB/frontend/bench_bnb.jsx
+++ BenchBnB/frontend/bench_bnb.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.APIUtil = APIUtil; 
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
