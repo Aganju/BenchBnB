79e5f24173b0e7732241375f1626bae53158a48e
Omayemi Agbeyegbe <grend500@gmail.com>
2017-08-18 17:33:05 -0400

    create session form for login and signup

--- BenchBnB/frontend/actions/session_actions.js
+++ BenchBnB/frontend/actions/session_actions.js
@@ -20,20 +20,20 @@ export const receiveErrors = (errors) => {
 export const login = (user) => (dispatch) => {
   return APIUtil.login(user)
     .then(user => dispatch(receiveCurrentUser(user)),
-        (errors) => dispatch(receiveErrors(errors))
+        (errors) => dispatch(receiveErrors(errors.responseJSON))
     );
 };
 
 export const signup = (user) => (dispatch) => {
   return APIUtil.signup(user)
       .then(user => dispatch(receiveCurrentUser(user)),
-      (errors) => dispatch(receiveErrors(errors))
+      (errors) => dispatch(receiveErrors(errors.responseJSON))
   );
 };
 
 export const logout = () => (dispatch) => {
   return APIUtil.logout()
       .then(() => dispatch(receiveCurrentUser(null)),
-        (errors) => dispatch(receiveErrors(errors))
+        (errors) => dispatch(receiveErrors(errors.responseJSON))
     );
 };
