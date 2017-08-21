cb6e36bb979fe03a12931bd9909073747f9bfff5
Omayemi Agbeyegbe <grend500@gmail.com>
2017-08-21 12:42:26 -0400

    add bench seeds and routes

--- BenchBnB/db/seeds.rb
+++ BenchBnB/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Bench.destroy_all
+User.destroy_all
+
+User.create(username: 'user', password: 'password')
+
+Bench.create(description: Faker::HitchhikersGuideToTheGalaxy.location,
+            lat: 37.767117, lng: -122.497347)
+Bench.create(description: Faker::HitchhikersGuideToTheGalaxy.location,
+            lat: 37.768881, lng: -122.496752)
+Bench.create(description: Faker::HitchhikersGuideToTheGalaxy.location,
+            lat: 37.749312, lng: -122.482963)
+Bench.create(description: Faker::HitchhikersGuideToTheGalaxy.location,
+            lat: 37.764179, lng: -122.427950)
+Bench.create(description: Faker::HitchhikersGuideToTheGalaxy.location,
+            lat: 37.739124, lng: -122.442513)
+Bench.create(description: Faker::HitchhikersGuideToTheGalaxy.location,
+            lat: 37.665554, lng: -122.405244)
+Bench.create(description: Faker::HitchhikersGuideToTheGalaxy.location,
+            lat: 37.799684, lng: -122.409108)
