c5f0a4f9b7697e6e217267b5977e3a47a8d1a750
Omayemi Agbeyegbe <grend500@gmail.com>
2017-08-21 17:15:09 -0400

    update backend to search by location

--- BenchBnB/app/models/bench.rb
+++ BenchBnB/app/models/bench.rb
@@ -1,3 +1,14 @@
 class Bench < ApplicationRecord
   validates :description, :lat, :lng, presence: true
+
+  def self.in_bounds(bounds)
+    north = bounds['northEast']['lat']
+    east = bounds['northEast']['lng']
+    south = bounds['southWest']['lat']
+    west = bounds['southWest']['lng']
+    Bench.where(
+      "lat > ? AND lat < ?
+      AND lng > ? AND lng < ?",
+      south, north, west, east )
+    end
 end
